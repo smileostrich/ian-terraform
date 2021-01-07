bb62ca349e11fe303ebd82a80120079dcb79797e
ilmin Moon <ian.ilminmoon@gmail.com>
2021-01-07 18:26:33 +0900

    for output iam user psswd(syntax error fixed)

--- global/output.tf
+++ global/output.tf
@@ -1,7 +1,13 @@
 output "encrypted_passwords" {
     description = "the encrypted password, base 64 encoded"
     value = {
-        for index, user in var.sysadmins:
-        user.username => aws_iam_user_login_profile.sysadmins.*.encrypted_password[index]
+        # for index, user in var.sysadmins:
+        # user.username => aws_iam_user_login_profile.sysadmins.*.encrypted_password[index]
+        for key in aws_iam_access_key.sysadmins :
+        key.user => {
+        aws_access_key_id               = key.id,
+        encrypted_aws_secret_access_key = key.encrypted_secret
+        encrypted_initial_password      = aws_iam_user_login_profile.sysadmins[key.user].encrypted_password
     }
+  }
 }
