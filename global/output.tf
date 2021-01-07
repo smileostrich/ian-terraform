289c5bfdf499a5c0ffc93eb042afb75381444b0d
ilmin Moon <ian.ilminmoon@gmail.com>
2021-01-07 18:22:32 +0900

    for output iam user psswd(syntax error fixed)

--- global/output.tf
+++ global/output.tf
@@ -1,4 +1,5 @@
 output "encrypted_passwords" {
+    description = "the encrypted password, base 64 encoded"
     value = {
         for index, user in var.sysadmins:
         user.username => aws_iam_user_login_profile.sysadmins.*.encrypted_password[index]
---
+++ global/variable.tf
@@ -0,0 +1,4 @@
+variable "sysadmins" {
+  description = "the encrypted password, base 64 encoded"
+  type        = string
+}
