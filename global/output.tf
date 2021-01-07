8f375506c45059649c0373b8a2f5f292069628ec
ilmin Moon <ian.ilminmoon@gmail.com>
2021-01-07 19:23:58 +0900

    for output iam user psswd

--- global/output.tf
+++ global/output.tf
@@ -1,8 +1,6 @@
-output "encrypted_passwords" {
+output "encrypted_aws_iam_secrets" {
     description = "the encrypted password, base 64 encoded"
     value = {
-        # for index, user in var.sysadmins:
-        # user.username => aws_iam_user_login_profile.sysadmins.*.encrypted_password[index]
         for key in aws_iam_access_key.sysadmins :
         key.user => {
         aws_access_key_id               = key.id,
